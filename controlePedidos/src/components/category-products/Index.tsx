17079f2f881c71a11a710937ebd461425fcc83d9
Leonardo Lopes de Oliveira <leonardo.loliveira@rede.ulbra.br>
2025-09-15 02:39:26 -0300

    arrumando style do componente de catetory

--- controlePedidos/src/components/category-products/Index.tsx
+++ controlePedidos/src/components/category-products/Index.tsx
@@ -1,35 +1,52 @@
-import { useState } from "react"
-import { Image, Text, TouchableOpacity, View } from "react-native"
-import ProductCategoryStyle from '../products-category/styles/style'
+import { useState } from "react";
+import { Image, Modal, Text, TouchableOpacity, View } from "react-native";
+import ProductCategoryStyle from "../products-category/styles/style";
+import ModalCategoryStyle from "../products-category/styles/styleModal";
 
 type Props = {
-    title: string,
-    urlImg: any
-}
+  title: string;
+  urlImg: any;
+};
 
-export const ProductCategory = ({title, urlImg}: Props) => {
+export const ProductCategory = ({ title, urlImg }: Props) => {
+  const [isModalVisible, setModalVisible] = useState<boolean>(false);
+  const open = () => setModalVisible(true);
+  const close = () => setModalVisible(false);
 
-    const [isModalVisible, setModalVisible] = useState(false)
-    const open = () => setModalVisible(true)
-    const close = () => setModalVisible(false)
-
-
-    return (
-        <View>
-            <TouchableOpacity style={ProductCategoryStyle.container} onPress={open}>
-                <View style={ProductCategoryStyle.containerImg}>
-                    <Image
-                        source={urlImg}
-                        style={ProductCategoryStyle.imgCategory}
-                    />
-                </View>
-
-                <View style={ProductCategoryStyle.containerTxt}>
-                    <Text style={ProductCategoryStyle.txtCategory}>{title}</Text>
-                </View>
-
-            </TouchableOpacity>
-
+  return (
+    <View>
+      <TouchableOpacity style={ProductCategoryStyle.container} onPress={open}>
+        <View style={ProductCategoryStyle.containerImg}>
+          <Image source={urlImg} style={ProductCategoryStyle.imgCategory} />
         </View>
-    )
-}
+
+        <View style={ProductCategoryStyle.containerTxt}>
+          <Text style={ProductCategoryStyle.txtCategory}>{title}</Text>
+        </View>
+
+        <Modal
+          visible={isModalVisible}
+          animationType="slide"
+          onRequestClose={close}
+        >
+          <View style={ModalCategoryStyle.container}>
+            <View style={ModalCategoryStyle.containerTxtCategory}>
+              <Text style={ModalCategoryStyle.txtTitleCategory}>{title}</Text>
+            </View>
+
+            <View style={ModalCategoryStyle.containerContent}></View>
+
+            <View style={ModalCategoryStyle.btnCloseContainer}>
+              <TouchableOpacity
+                onPress={close}
+                style={ModalCategoryStyle.btnClose}
+              >
+                <Text style={ModalCategoryStyle.txtBtnClose}>FECHAR</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </TouchableOpacity>
+    </View>
+  );
+};
--- controlePedidos/src/components/products-category/styles/styleModal.tsx
+++ controlePedidos/src/components/products-category/styles/styleModal.tsx
@@ -1,43 +1,65 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        elevation: 10,
-        width: '100%',
-        maxHeight: '95%',
-        overflow: 'scroll',
-        paddingTop: 20,
-        zIndex: 5,
-        position: 'relative'
+  container: {
+    flex: 1,
 
-    },
+    alignItems: "center",
+    backgroundColor: "white",
+    
+    elevation: 10,
+    width: "100%",
+    maxHeight: "95%",
+    overflow: "scroll",
 
-    containerContent: {
-        width: '100%',
-        height: '80%'
+    zIndex: 5,
+    position: "relative",
+  },
 
-    },
+  containerTxtCategory: {
+    width: "100%",
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
 
-    containerBtn: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        height: '20%',
-        alignItems: 'center',
-        justifyContent: 'center'
+  txtTitleCategory: {
+    fontSize: 25,
+    fontWeight: "500",
+  
+  },
 
-    },
+  containerContent: {
+    width: "100%",
+    height: "80%",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.1)",
+  },
 
-    btnClose: {
-        width: 120,
-        height: 20,
-        backgroundColor: 'blue'
-    }
-})
+  btnCloseContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: "50%",
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  btnClose: {
+    backgroundColor: "red",
+    width: 200,
+    height: 60,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 2,
+  },
+
+  txtBtnClose: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
